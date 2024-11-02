8e935742cf8a79be528cce12a802c571ba051cb0
joyj10 <micool1030@gmail.com>
2024-11-03 08:14:57 +0900

    [setting] 시큐리티 및 설정 파일 적용 - 테스트 위한 시큐리티 임시 허용 - 데이터베이스 설정 추가

---
+++ src/main/java/com/large/board/config/WebSecurityConfig.java
@@ -0,0 +1,48 @@
+package com.large.board.config;
+
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    private static final String[] SWAGGER = {
+            "/swagger-ui.html",
+            "/swagger-ui/**",
+            "/v3/api-docs/**"
+    };
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .csrf(AbstractHttpConfigurer::disable) //csrf 공격 보안 disable 처리 (csrf Cross Site Forgery 사이트 간 요청 위조)
+                .sessionManagement(sessionManagement -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // 세션 생성 정책 : RESTful API 적합한 설정
+                .authorizeHttpRequests(authorizationManagerRequestMatcherRegistry ->
+                        authorizationManagerRequestMatcherRegistry
+                                // static resou미rce 의 경우 모든 요청 허용 처리
+                                .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+
+                                // swagger 인증 없이 통과 처리
+                                .requestMatchers(SWAGGER).permitAll()
+
+                                // open-api 하위 모든 주소는 인증 없이 통과
+                                .requestMatchers("/open-api/**").permitAll()
+
+                                // API 시큐리티 인증 없이 통과 (임시 허용)
+                                .requestMatchers("/**").permitAll()
+
+                                // 그 외 모든 요청 인증 사용
+                                .anyRequest().authenticated()
+                )
+                .formLogin(AbstractHttpConfigurer::disable);
+
+        return httpSecurity.build();
+    }
+}
